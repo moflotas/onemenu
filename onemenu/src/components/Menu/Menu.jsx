912cde7465f502e07fdf5c6595015d7082aff007
nbogdankov@gmail.com <nbogdankov@gmail.com>
2023-07-02 21:18:19 +0300

    add first version of the popup

--- onemenu/src/components/Menu/Menu.jsx
+++ onemenu/src/components/Menu/Menu.jsx
@@ -3,26 +3,53 @@ import styles from './Menu.module.scss'
 import Card from "./Card/Card";
 import TextCard from "./Card/textCard";
 import cn from "classnames"
+import Popup from "./Popup/Popup";
+
 
 
 
 const Menu = () => {
+
     let data = [
         {category: "Fried fish", items: [
-            {name: "tasty food", price: 255},
-            {name: "Something different", price: 567},
-            {name: "Culture", price: 12},
-            {name: "Lunch", price: 450},
-            {name: "fruit", price: 216},
-            {name: "aaaa", price: 215}
+            {id: 1, name: "tasty food", price: 255, weight: 320, ingredients: [
+                {name: "carrot"}, {name: "potato"}, {name: "something"}
+            ]},
+            {id: 2, name: "Something different", price: 567, weight: 250, ingredients: [
+                {name: "carrot"}, {name: "potato"}, {name: "something"}
+            ]},
+            {id: 3, name: "Culture", price: 12, weight: 250, ingredients: [
+                {name: "carrot"}, {name: "potato"}, {name: "something"}
+            ]},
+            {id: 4, name: "Lunch", price: 450, weight: 250, ingredients: [
+                {name: "carrot"}, {name: "potato"}, {name: "something"}
+            ]},
+            {id: 5, name: "fruit", price: 216, weight: 250, ingredients: [
+                {name: "carrot"}, {name: "potato"}, {name: "something"}
+            ]},
+            {id: 6, name: "aaaa", price: 215, weight: 250, ingredients: [
+                {name: "carrot"}, {name: "potato"}, {name: "something"}
+            ]}
         ], category_id: 1},
         {category: "Fried potato", items: [
-            {name: "tasty food", price: 255},
-            {name: "Something different", price: 567},
-            {name: "Culture", price: 12},
-            {name: "Lunch", price: 450},
-            {name: "fruit", price: 216},
-            {name: "aaaa", price: 215}
+            {id: 7, name: "tasty food", price: 255, weight: 250, ingredients: [
+                {name: "carrot"}, {name: "potato"}, {name: "something"}
+            ]},
+            {id: 8, name: "Something different", price: 567, weight: 250, ingredients: [
+                {name: "carrot"}, {name: "potato"}, {name: "something"}
+            ]},
+            {id: 9, name: "Culture", price: 12, weight: 250, ingredients: [
+                {name: "carrot"}, {name: "potato"}, {name: "something"}
+            ]},
+            {id: 10, name: "Lunch", price: 450, weight: 250, ingredients: [
+                {name: "carrot"}, {name: "potato"}, {name: "something"}
+            ]},
+            {id: 11, name: "fruit", price: 216, weight: 250, ingredients: [
+                {name: "carrot"}, {name: "potato"}, {name: "something"}
+            ]},
+            {id: 12, name: "aaaa", price: 215, weight: 250, ingredients: [
+                {name: "carrot"}, {name: "potato"}, {name: "something"}
+            ]}
         ], category_id: 2}
     ]
 
@@ -36,7 +63,14 @@ const Menu = () => {
         setHasImage(true);
       };
         
+    const [isOpen, setIsOpen] = useState(false)
 
+    const [popupItem, setPopupItem] = useState()
+
+    const togglePopup = (item) => {
+        setIsOpen(!isOpen)
+        setPopupItem(item)
+    }
 
     return (
         <div className={styles.menu}>
@@ -68,17 +102,24 @@ const Menu = () => {
                     <rect y="12" width="22" height="4" rx="2" fill="currentColor"/>
                     <rect y="18" width="22" height="4" rx="2" fill="currentColor"/>
                 </svg>
-
                 </button>
-            </div>
+            </div> 
+
+            {isOpen && (
+                <Popup popupItem={popupItem} />
+            )}
 
             {data.map((category) => (
                 <div className={styles.category}>
                     <span id={category.category_id} className={styles.category_title}>{category.category}</span>
                     <div className={styles.cards}>
                         {category.items.map((item) => (
-                            hasImage ? (<Card name={item.name} price={item.price}/>)
-                            : (<TextCard name={item.name} price={item.price} />)
+                            hasImage ? 
+                            ( <button onClick={() => togglePopup(item)} className={styles.buttonCard}>
+                                <Card name={item.name} price={item.price}/>
+                            </button> )
+                            :
+                            (<TextCard name={item.name} price={item.price} /> )
                         ))}
                     </div>
                 </div>
---
+++ onemenu/src/components/Menu/Popup/Popup.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./Popup.module.scss"
+import foodPicture from '../../../pictures/foodLarge.png'
+
+const Popup = (props) => {
+    return (
+        <div className={styles.popup}>
+            <div className={styles.content}>
+                <div className={styles.inside}>
+                    <div className={styles.picture}>
+                        <img className={styles.image} src={foodPicture} alt="" />
+                    </div>
+                    <div className={styles.title_block}>
+                        <span className={styles.title}> {props.popupItem.name} </span>
+                        <span className={styles.weight}> {props.popupItem.weight}g </span>
+                        <span className={styles.description}> Here is the description of the product. Here is the description of the dish, and it states that this salad is not tasty. And it is definitely true. </span>
+                    </div>
+                    <div className={styles.base_ingredients}>
+                        <span className={styles.base_title}>Base ingredients</span>
+                        {props.popupItem.ingredients.map((ingredient) => (
+                            <span className={styles.ingredient}>{ingredient.name}</span>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Popup;
