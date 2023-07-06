a058bff3e7a9f3ec82b5f57bc3a076ce181d710a
nbogdankov@gmail.com <nbogdankov@gmail.com>
2023-07-06 15:02:28 +0300

    fixed

--- onemenu/src/components/Menu/Cards/Card/textCard.jsx
+++ onemenu/src/components/Menu/Cards/Card/textCard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styles from "./Card.module.scss"
 import { arrowForward } from "../../../../pictures/svg";
-
+import cn from "classnames";
 
 
 const TextCard = ({item, togglePopup}) => {
     return (
         <div className={styles.textCard}>
-            <div className={styles.textCard_title}>
+            <div className={cn(styles.textCard_title, styles.textCard_title_mb)}>
                 <span>{item.name}</span>
             </div>
             <div className={styles.textCard_content}>
--- onemenu/src/components/Menu/Cards/Cards.jsx
+++ onemenu/src/components/Menu/Cards/Cards.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import styles from "./Cards.module.scss"
 import Card from "./Card/Card";
 import TextCard from "./Card/textCard";
-
+import cn from "classnames"
 
 const Cards = ({data, hasImage, togglePopup}) => {
     return (
         <div>
             {data.map((category) => (
-                <div className={styles.category}>
+                <div className={cn(styles.category, {[styles.mb] : !hasImage})}>
                     <span id={category.category_id} className={styles.category_title}>{category.category}</span>
                     <div className={styles.cards}>
                         {category.items.map((item) => (
