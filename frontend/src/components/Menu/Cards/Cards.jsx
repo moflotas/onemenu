77913c6b64871b1de4ff10b6c688eef8e5a29ff0
nbogdankov@gmail.com <nbogdankov@gmail.com>
2023-07-13 00:31:55 +0300

    added categories

--- frontend/src/components/Menu/Cards/Cards.jsx
+++ frontend/src/components/Menu/Cards/Cards.jsx
@@ -4,39 +4,54 @@ import Card from "./Card/Card";
 import TextCard from "./Card/textCard";
 import cn from "classnames";
 
-const Cards = ({ data, hasImage, togglePopup, cafe }) => {
-  return (
-    <div className={styles.cards} >
-      {cafe && cafe.menu && cafe.menu.map((item) => (
-          hasImage ? (
-            <button onClick={() => togglePopup(item)} key={item.id} className={styles.buttonCard}>
-              <Card name={item.name} price={item.cost} />
-            </button>
-          ) : (
-            <TextCard key={item.id} item={item} togglePopup={togglePopup} />
-          )
-      ))}
-      {/* {data.map((category) => (
-        <div key={category.category_id} className={cn(styles.category, {[styles.mb] : !hasImage})}>
-          <span id={category.category_id} className={styles.category_title}>
-            {category.category}
-          </span>
-          <div className={styles.cards}>
-            {category.items.map((item) => (
-              hasImage ? 
-              (
-                <button key={item.id} onClick={() => togglePopup(item)} className={styles.buttonCard}>
-                  <Card name={item.name} price={item.price} />
-                </button>
-              ) : (
-                <TextCard key={item.id} item={item} togglePopup={togglePopup} />
-              )
-            ))}
-          </div>
-        </div>
-      ))} */}
-    </div>
-  );
+const Cards = ({ hasImage, togglePopup, cafe }) => {
+	const groupedMenu =
+		cafe && cafe.menu
+			? cafe.menu.reduce((groups, item) => {
+					const category = item.category;
+					if (!groups[category]) {
+						groups[category] = [];
+					}
+					groups[category].push(item);
+					return groups;
+			  }, {})
+			: {};
+
+	return (
+		<div>
+			{Object.keys(groupedMenu).map((category) => (
+				<div
+					key={category}
+					className={cn(styles.category, { [styles.mb]: !hasImage })}
+				>
+					<span className={styles.category_title}>{category}</span>
+					<div
+						className={cn(styles.cards, {
+							[styles.no_gap]: !hasImage,
+						})}
+					>
+						{groupedMenu[category].map((item) =>
+							hasImage ? (
+								<button
+									onClick={() => togglePopup(item)}
+									key={item.id}
+									className={styles.buttonCard}
+								>
+									<Card name={item.name} price={item.cost} />
+								</button>
+							) : (
+								<TextCard
+									key={item.id}
+									item={item}
+									togglePopup={togglePopup}
+								/>
+							)
+						)}
+					</div>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Cards;
