36e3cc7eafea423ea7f626f6a88eb758b3b02946
nbogdankov@gmail.com <nbogdankov@gmail.com>
2023-07-17 18:42:27 +0300

    fill

--- backend/db/crud/dev.py
+++ backend/db/crud/dev.py
@@ -2,11 +2,13 @@ from db.session import engine
 import db.models as models
 import api.schemas as schemas
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.schema import DropTable
 
 
 async def _db_recreate():
     async with engine.begin() as conn:
-        await conn.run_sync(models.Base.metadata.drop_all)
+        for table in reversed(models.Base.metadata.sorted_tables):
+            conn.execute(DropTable(table, if_exists=True))
         await conn.run_sync(models.Base.metadata.create_all)
     await engine.dispose()
 
